9f5f2f3d75b534fb3b18c1d17226c2d820cf2477
PEDRO HENRIQUE VIEIRA SILVA <22102416@CFP127ALUNO.LOCAL>
2022-05-13 11:23:04 -0300

    Slider quase pronto

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 const list = document.querySelector('#cards');
 const next = document.querySelector('#next');
 const prev = document.querySelector('#prev');
-let count = -1;
+
+let count = 2;
+list.children[count - 1].classList.add('maior');
 
 prev.addEventListener('click', function () {
   console.log(count)
@@ -15,11 +17,13 @@ prev.addEventListener('click', function () {
 
 next.addEventListener('click', function () {
   count = count + 1;
-  console.log(count)
-  console.log(list.children[count]);
+  // console.log(count)
+  // console.log(list.children[count]);
   list.children[count].classList.add('cards-visiveis');
+  list.children[count - 1].classList.add('maior');
 
-  if (count > 2) {
+  if (count > 2) {  
+    list.children[count - 2].classList.remove('maior');
     list.children[count - 3].classList.remove('cards-visiveis');
   }
 });
